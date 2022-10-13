7e42e6f88710024be75cf3762ddbb413ecae4422
Philips Ucheolisah Nge <u.phils.nge@gmail.com>
2022-10-13 10:20:11 -0400

    init

---
+++ android/app/src/main/kotlin/com/phixlab/money/phixlab_money/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.phixlab.money.phixlab_money
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+/// Colors
+const Color primaryDark = Color(0xFF40ACA1);
+const Color primaryLight = Color(0xFF6CDAFF);
+
+const Color secondaryDark = Color(0xFF507DFF);
+const Color secondaryLight = Color(0xFF60A6FF);
+
+const Color fontDark = Color(0xFF5C6E8D);
+const Color fontLight = Color(0xFFB3C7E0);
+
+const Color accent = Color(0xFF8A4D52);
+const Color background = Color(0xFFF1F2F5);
+
+/// dimensions
+const double defaultSpacing = 16.0;
+const double defaultRadius = 12.0;
+
+const double fontSizeHeading = 18.0;
+const double fontSizeTitle = 16.0;
+const double fontSizeBody = 13.0;
+
